a3b9e9f9c75c2e4f16df317ffb0f1296812a1d1d
RajRonal <ronal.raj@remotestate.com>
2024-01-26 02:11:02 +0530

    test

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+def load_config():
+    branch = os.environ.get('BRANCH', 'main')
+    if branch == "main":
+        filename = 'config_production.yaml'
+    elif branch == "dev":
+        filename = 'config_staging.yaml'
+    else:
+        raise ValueError(f"Unsupported branch: {branch}")
+
+    with open(filename, 'r') as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+config = load_config()
+
+    # ... rest of the code
+
+    # Use the LB DNS from the config
+lb_dns = config['lb_dns']
+print(lb_dns)
